f536b42115970fe9fa9ba3f500c38f1017d40bf3
Kim <nicole.kim@accenture.com>
2024-06-15 00:54:55 -0700

    define mongoose models

---
+++ backend/src/models.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const photoSchema = new Schema({
+  url: String,
+  caption: String,
+  generatedCaption: String,
+});
+
+// need to convert schema into a model with which we can work
+const Photo = mongoose.model("Photo", photoSchema);
