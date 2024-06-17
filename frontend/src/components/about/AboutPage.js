d6882ae4582c536f25ae1cdb644a3a4a1bdf9a9a
Kim <nicole.kim@accenture.com>
2024-06-17 16:20:40 -0700

    init about page

---
+++ frontend/src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This app uses React, Redux, React Router, and many other helpful
+      libraries. Mainly just adding bits and pieces of programming languages,
+      libraries, frameworks, tools, etc. to practice my coding skills. \n Ask me
+      for access to my private github repo for detailed README.md documents.
+    </p>
+  </div>
+);
+
+export default AboutPage;
