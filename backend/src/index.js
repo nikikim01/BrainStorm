271d55f788dd8f1f5035a6b1e354cb5dd02c3b84
Kim <nicole.kim@accenture.com>
2024-06-16 23:19:47 -0700

    working graphql running on server localhost 4000

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,38 @@
+import http from "node:http";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import express from "express";
-import bodyParser from "body-parser";
-
+import { schema } from "./schema.js";
 import {
   connect as dbConnect,
   disconnect as dbDisconnect,
 } from "./dbClient.js";
 
-async function startServer() {
+const startServer = async () => {
   const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
 
-  app.use(bodyParser.json());
+  app.use("/graphql", express.json(), expressMiddleware(server));
 
-  try {
-    await dbConnect();
-    console.log("Successful connection!");
-  } catch (err) {
-    res.status(500).send(err);
-  } finally {
+  await dbConnect();
+
+  httpServer.listen(4000, () => {
+    console.log("Server is ready at http://localhost:4000/graphql !");
+  });
+
+  process.on("SIGINT", async () => {
+    console.log("Closing HTTP server");
     await dbDisconnect();
-  }
-}
+    httpServer.close();
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
+};
 
 startServer();
