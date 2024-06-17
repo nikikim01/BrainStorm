98d76e73a32f296f182b159a492f67e5b143a651
Kim <nicole.kim@accenture.com>
2024-06-17 16:24:16 -0700

    reinit index.js to use app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import App from "./components/App";
+import "./index.css";
 
-const sayHello = () => {
-  return <h1>Hi</h1>;
-};
-
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<React.StrictMode>{sayHello()}</React.StrictMode>);
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
