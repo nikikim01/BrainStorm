0619b761359437f5b2de15f858ffd2aff06862c9
Kim <nicole.kim@accenture.com>
2024-06-18 08:58:46 -0700

    restructure photos directory

---
+++ frontend/src/components/photos/photo/Photo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Photo = ({ Photo }) => {
+  <div className="Photo-container">
+    <img src={Photo.url} alt={Photo.caption} className="Photo-image" />
+    <div className="Photo-caption">
+      <p>{Photo.caption}</p>
+    </div>
+    <div className="Photo-generatedCaption">
+      <p>{Photo.generatedCaption}</p>
+    </div>
+  </div>;
+};
+
+export default Photo;
